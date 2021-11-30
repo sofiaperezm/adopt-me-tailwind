7fbfbe2e2e46a69fcdf0c261597d11b388e4cdcd
sofiaperezm <sofi.prz.m@gmail.com>
2021-11-30 12:04:52 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import { StrictMode, useState } from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+import ThemeContext from "./ThemeContext";
+
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", { id: "my-brand" }, "Adopt me!"),
+//     React.createElement(Pet, {
+//       name: "Levi",
+//       animal: "cat",
+//       breed: "domestic longhair cat",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Kira!!",
+//       animal: "cat",
+//       breed: "Domestic short-haired cat",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Flaca",
+//       animal: "dog",
+//       breed: "mixed-breed",
+//     }),
+//   ]);
+// };
+
+const App = () => {
+  const theme = useState("darkblue");
+  return (
+    <ThemeContext.Provider value={theme}>
+      <div>
+        <Router>
+          <header>
+            <Link to="/">
+              <h1>Adopt me!</h1>
+            </Link>
+          </header>
+          <Switch>
+            <Route path="/">
+              <SearchParams />
+            </Route>
+            <Route path="/details/:id">
+              <Details />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </ThemeContext.Provider>
+  );
+};
+
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
---
+++ src/SearchParams.js
@@ -0,0 +1,102 @@
+import { useState, useEffect, useContext } from "react";
+import ThemeContext from "./ThemeContext";
+import useBreedList from "./useBreedList";
+import Results from "./Results";
+
+const ANIMALS = ["dog", "cat", "rabbit", "bird", "reptile"];
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
+  const [breeds] = useBreedList(animal);
+  const [theme, setTheme] = useContext(ThemeContext);
+
+  useEffect(() => {
+    requestPets();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+    setPets(json.pets);
+  }
+
+  //   function updateLocation(event) {
+  //     setLocation(event.target.value);
+  //   }
+
+  return (
+    <div className="search-params">
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          requestPets();
+        }}
+      >
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            onChange={(event) => setLocation(event.target.value)}
+            // onChange={updateLocation}
+            value={location}
+            placeholder="Location"
+          ></input>
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(event) => setAnimal(event.target.value)}
+            onBlur={(event) => setAnimal(event.target.value)}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(event) => setBreed(event.target.value)}
+            onBlur={(event) => setBreed(event.target.value)}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option value={breed} key={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="theme">
+          Theme
+          <select
+            value={theme}
+            onChange={(event) => setTheme(event.target.value)}
+            onBlur={(event) => setTheme(event.target.value)}
+          >
+            <option value="darkblue">Dark Blue</option>
+            <option value="MediumTurquoise">Medium Turquoise</option>
+            <option value="Plum">Plum</option>
+            <option value="PeachPuff">Peach Puff</option>
+          </select>
+        </label>
+        <button style={{ backgroundColor: theme }}>Submit</button>
+      </form>
+      <Results pets={pets} />
+    </div>
+  );
+};
+
+export default SearchParams;
