bed8e5b1841f5670bb0e277aea797dd4d6729f68
sofiaperezm <sofi.prz.m@gmail.com>
2021-12-01 15:02:45 -0500

    tailwind

--- src/App.js
+++ src/App.js
@@ -38,8 +38,8 @@ const App = () => {
         }}
       >
         <Router>
-          <header>
-            <Link to="/">
+          <header className="w-full mb-10 text-center p-7 bg-gradient-to-b from-purple-400 via-pink-500 to-red-500">
+            <Link className="text-6xl text-white hover:text-gray-200" to="/">
               <h1>Adopt me!</h1>
             </Link>
           </header>
