206cc4d3ca2530f71b91eb29d4876493f3fa9ff7
sofiaperezm <sofi.prz.m@gmail.com>
2021-11-30 23:31:13 -0500

    added tailwind into the project

--- src/App.js
+++ src/App.js
@@ -30,7 +30,13 @@ const App = () => {
   const theme = useState("darkblue");
   return (
     <ThemeContext.Provider value={theme}>
-      <div>
+      <div
+        className="p-0 m-0"
+        style={{
+          background:
+            "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)",
+        }}
+      >
         <Router>
           <header>
             <Link to="/">
@@ -38,11 +44,11 @@ const App = () => {
             </Link>
           </header>
           <Switch>
-            <Route path="/">
-              <SearchParams />
-            </Route>
             <Route path="/details/:id">
               <Details />
+            </Route>
+            <Route path="/">
+              <SearchParams />
             </Route>
           </Switch>
         </Router>
